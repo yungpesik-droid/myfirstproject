d939cf1f96bdea133c22d2322ab5e336b4453bda
yungpesik-droid <75543265+yungpesik-droid@users.noreply.github.com>
2020-12-06 02:17:49 +0700

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fcb7cca057d16001e793bbe",
+				"5fcb7cca057d16001e793bc0"
+			]
+		},
+		"5fcb7cca057d16001e793bbe": {
+			"id": "5fcb7cca057d16001e793bbe",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fcb7cca057d16001e793bc0": {
+			"id": "5fcb7cca057d16001e793bc0",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
